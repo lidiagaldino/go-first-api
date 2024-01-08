e8ba548aa5aaa8a5fff10c067a50c3398dc1ccd8
lidiagaldino <lidiagabrielly14@gmail.com>
2024-01-07 22:20:18 -0300

    add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/lidiagaldino/go-first-api/router"
+import (
+	"github.com/lidiagaldino/go-first-api/config"
+	"github.com/lidiagaldino/go-first-api/router"
+)
+
+var(
+    logger config.Logger
+)
 
 func main() {
+    logger = *config.GetLogger("main")
+    err := config.Init()
+    if err != nil {
+        logger.Errorf("config initialization error: %v", err)
+        return
+    }
+
    router.Initialize()
 }
