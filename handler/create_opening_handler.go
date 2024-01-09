0efb21786bdf031993626c3837fb37d6af909b37
lidiagaldino <lidiagabrielly14@gmail.com>
2024-01-08 22:33:55 -0300

    add request and response helpers and add create_opening logic

--- handler/create_opening_handler.go
+++ handler/create_opening_handler.go
@@ -4,11 +4,33 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lidiagaldino/go-first-api/schemas"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST opening",
-		"status": 200,
-	})
+	request := CreateOpeningRequest{}
+	
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("error validating request: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error creating opening")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
---
+++ handler/response.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{"error": msg, "errorCode": code})
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfully", op), "data": data, 
+	})
+} 
